399ccf3b17e3b76a25c2c7db237eb039a8dfe27a
tm <tommertensjobapplication@gmail.com>
2020-06-04 14:12:18 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+from setuptools import setup, find_packages
+
+setup(
+    name='intake-latticejson',
+    version='0.0.1',
+    description='Test plugin: latticejson',
+    url='',
+    maintainer='Trainee',
+    maintainer_email='',
+    license='BSD',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['intake'],
+    long_description="",
+    zip_safe=False,
+)
+
+
